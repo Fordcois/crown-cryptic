65dd4d52d441cc09a5f0eef62d12b1d5f2f77447
Sam Ford <samueljamesford@googlemail.com>
2025-11-25 07:40:12 +0000

    Add fonts and colours to keyboard

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,5 @@
-import { Inter } from "next/font/google";
 import Header from "@/components/header";
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Crown Cryptic",
@@ -17,7 +14,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
       <Header/>
         {children}
         </body>
